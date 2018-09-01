5cbfe42bf3fadf48aec86633708719e4014f0272
duduluu <duduluu@outlook.com>
2018-09-02 03:05:58 +0800

    init project

---
+++ packages/@void/vue-markdown-loader/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  collectCoverage: true,
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.ts|**/*.(spec|test).(js|ts)'],
+  testURL: 'http://localhost/',
+};
