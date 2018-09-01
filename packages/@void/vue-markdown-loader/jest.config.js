fbfbff1c9379ec588657464cc02a8070e2ad754e
duduluu <duduluu@outlook.com>
2018-09-02 06:56:46 +0800

    update frame

--- packages/@void/vue-markdown-loader/jest.config.js
+++ packages/@void/vue-markdown-loader/jest.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  collectCoverage: true,
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testMatch: ['**/tests/**/*.ts|**/*.(spec|test).(js|ts)'],
-  testURL: 'http://localhost/',
-  moduleDirectories: ['node_modules', '../../'],
-};
+module.exports = require('../../../jest.config');
