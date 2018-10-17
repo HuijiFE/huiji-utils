7ba6e932589ec0e31b6ffec43e04317700531f1d
duduluu <duduluu@outlook.com>
2018-10-17 18:39:11 +0800

    [graphql-tools] fix: type declarations

--- packages/@huiji/graphql-tools/src/introspection.ts
+++ packages/@huiji/graphql-tools/src/introspection.ts
@@ -110,37 +110,80 @@ fragment TypeRef on __Type {
  * as well as the entry points for query, mutation, and subscription operations.
  */
 export interface __Schema {
-  types: __Type[];
-  queryType: __Type;
+  /**
+   * A list of all directives supported by this server.
+   */
+  directives: (__Directive)[];
+
+  /**
+   * If this server supports mutation, the type that mutation operations will be rooted at.
+   */
   mutationType: __Type | null;
+
+  /**
+   * The type that query operations will be rooted at.
+   */
+  queryType: __Type;
+
+  /**
+   * If this server support subscription, the type that subscription operations will be rooted at.
+   */
   subscriptionType: __Type | null;
-  directives: __Directive[];
+
+  /**
+   * A list of all types supported by this server.
+   */
+  types: (__Type)[];
 }
 
 /**
  * The fundamental unit of any GraphQL Schema is the type.
- * There are many kinds of types in GraphQL as represented by the __TypeKind enum.
+ * There are many kinds of types in GraphQL as represented by the `__TypeKind` enum.
  *
- * Depending on the kind of a type, certain fields describe information about that type.
- * Scalar types provide no information beyond a name and description, while Enum types provide their values.
- * Object and Interface types provide the fields they describe.
- * Abstract types, Union and Interface, provide the Object types possible at runtime.
+ * Depending on the kind of a type,
+ * certain fields describe information about that type.
+ * Scalar types provide no information beyond a name and description,
+ * while Enum types provide their values.
+ * Object and Interface types provide the fields they describe. Abstract types,
+ * Union and Interface, provide the Object types possible at runtime.
  * List and NonNull types compose other types.
  */
 export interface __Type {
-  kind: __TypeKind;
-  name: string | null;
   description: string | null;
-  fields: __Field[] | null;
-  interfaces: __Type[] | null;
-  possibleTypes: __Type[] | null;
-  enumValues: __EnumValue[] | null;
-  inputFields: __InputValue[] | null;
+
+  /**
+   * ```
+   * function enumValues(
+   *   includeDeprecated?: boolean,
+   * ): (__EnumValue)[];
+   * ```
+   */
+  enumValues: (__EnumValue)[];
+
+  /**
+   * ```
+   * function fields(
+   *   includeDeprecated?: boolean,
+   * ): (__Field)[];
+   * ```
+   */
+  fields: (__Field)[];
+
+  inputFields: (__InputValue)[];
+
+  interfaces: (__Type)[];
+
+  kind: __TypeKind;
+
+  name: string | null;
+
   ofType: __Type | null;
+
+  possibleTypes: (__Type)[];
 }
 
 /**
- * An enum describing what kind of type a given __Type is.
+ * An enum describing what kind of type a given `__Type` is.
  */
 export enum __TypeKind {
   /**
@@ -149,37 +192,37 @@ export enum __TypeKind {
   SCALAR = 'SCALAR',
 
   /**
-   * Indicates this type is an object. fields and interfaces are valid fields.
+   * Indicates this type is an object. `fields` and `interfaces` are valid fields.
    */
   OBJECT = 'OBJECT',
 
   /**
-   * Indicates this type is an interface. fields and possibleTypes are valid fields.
+   * Indicates this type is an interface. `fields` and `possibleTypes` are valid fields.
    */
   INTERFACE = 'INTERFACE',
 
   /**
-   * Indicates this type is a union. possibleTypes is a valid field.
+   * Indicates this type is a union. `possibleTypes` is a valid field.
    */
   UNION = 'UNION',
 
   /**
-   * Indicates this type is an enum. enumValues is a valid field.
+   * Indicates this type is an enum. `enumValues` is a valid field.
    */
   ENUM = 'ENUM',
 
   /**
-   * Indicates this type is an input object. inputFields is a valid field.
+   * Indicates this type is an input object. `inputFields` is a valid field.
    */
   INPUT_OBJECT = 'INPUT_OBJECT',
 
   /**
-   * Indicates this type is a list. ofType is a valid field.
+   * Indicates this type is a list. `ofType` is a valid field.
    */
   LIST = 'LIST',
 
   /**
-   * Indicates this type is a non-null. ofType is a valid field.
+   * Indicates this type is a non-null. `ofType` is a valid field.
    */
   NON_NULL = 'NON_NULL',
 }
@@ -189,12 +232,17 @@ export enum __TypeKind {
  * each of which has a name, potentially a list of arguments, and a return type.
  */
 export interface __Field {
-  name: string;
-  description: string | null;
-  args: __InputValue[];
-  type: __Type;
-  isDeprecated: boolean;
+  args: (__InputValue)[];
+
   deprecationReason: string | null;
+
+  description: string | null;
+
+  isDeprecated: boolean;
+
+  name: string;
+
+  type: __Type;
 }
 
 /**
@@ -202,41 +250,57 @@ export interface __Field {
  * are represented as Input Values which describe their type and optionally a default value.
  */
 export interface __InputValue {
-  name: string;
-  description: string | null;
-  type: __Type;
   /**
    * A GraphQL-formatted string representing the default value for this input value.
    */
   defaultValue: string | null;
+
+  description: string | null;
+
+  name: string;
+
+  type: __Type;
 }
 
 /**
- * One possible value for a given Enum. Enum values are unique values,
+ * One possible value for a given Enum.
+ * Enum values are unique values,
  * not a placeholder for a string or numeric value.
  * However an Enum value is returned in a JSON response as a string.
  */
 export interface __EnumValue {
-  name: string;
-  description: string | null;
-  isDeprecated: boolean;
   deprecationReason: string | null;
+
+  description: string | null;
+
+  isDeprecated: boolean;
+
+  name: string;
 }
 
 /**
  * A Directive provides a way to describe alternate runtime execution
  * and type validation behavior in a GraphQL document.
  *
- * In some cases, you need to provide options to alter GraphQL's execution behavior
- * in ways field arguments will not suffice,
+ * In some cases, you need to provide options
+ * to alter GraphQL's execution behavior in ways field arguments will not suffice,
  * such as conditionally including or skipping a field.
  * Directives provide this by describing additional information to the executor.
  */
 export interface __Directive {
+  args: (__InputValue)[];
+
+  description: string | null;
+
+  locations: (__DirectiveLocation)[];
+
   name: string;
-  description?: string;
-  locations: __DirectiveLocation[];
-  args: __InputValue[];
+
+  onField: boolean;
+
+  onFragment: boolean;
+
+  onOperation: boolean;
 }
 
 /**
